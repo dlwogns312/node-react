bab148800e4e6944b933f63cd39d0da9e7a99515
dlwogns312 <dlwogns312@gmail.com>
2022-01-28 15:48:07 +0900

    처음 저장소

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://dlwogns312:dlqhdtjq5@coje.egm6j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+{
+ 
+},
+)
+.then(()=>console.log('MongoDB Coonected...'))
+.catch(err=>console.log(err))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => { 
+  console.log(`Example app listening on port ${port}`)
+})
