a8af5513cec4707613ab2eda9eb2a21a28d1d704
dlwogns312 <dlwogns312@gmail.com>
2022-02-16 18:40:06 +0900

    Login Page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -8,7 +8,12 @@ function LandingPage() {
     .then(response=>console.log(response.data))
   },[])
   return (
-    <div>LandingPage</div>
+    <div style={{
+      display : 'flex',justifyContent: 'center', alignItems: 'center',
+      width: '100%', height: '100vh'
+    }}>
+      <h2>시작 페이지</h2>
+     </div>
   )
 }
 
