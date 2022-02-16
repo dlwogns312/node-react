1b8c0ccf4b651d8d1770ea94a902531ca82f47f3
dlwogns312 <dlwogns312@gmail.com>
2022-02-16 20:28:30 +0900

    Login

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,17 +1,53 @@
-import React from 'react'
+import { Axios } from 'axios'
+import React, { useState } from 'react'
+import {useDispatch} from 'react-redux';
+import {loginUser} from '../../../_actions/user_action';
+import {useNavigate} from 'react-router-dom'
 
-function LoginPage() {
+function LoginPage(props) {
+
+  const dispatch = useDispatch();
+  const [Email, setEmail] = useState("")
+  const [Password, setPassword] = useState("")
+  let navigate = useNavigate();
+
+  const onEmailHandler = (event) =>{
+    setEmail(event.currentTarget.value)
+  }
+
+  const onPasswordHandler = (event) =>{
+    setPassword(event.currentTarget.value)
+  }
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault(); //할때마다 refresh되기때문에 
+
+    let body={
+      email: Email,
+      password: Password
+    }
+    dispatch(loginUser(body))
+      .then(response=>{
+        if(response.payload.loginSuccess){
+          navigate('/')
+        }else{
+          alert('Error')
+        }
+      })
+  }
   return (
     <div  style={{
       display : 'flex',justifyContent: 'center', alignItems: 'center',
       width: '100%', height: '100vh'
     }}>
 
-      <form style= {{ display:'flex', flexDirection: 'column'}}>
+      <form style= {{ display:'flex', flexDirection: 'column'}}
+        onSubmit={onSubmitHandler}
+      >
         <label>Email</label>
-        <input type="email" value onChange/>
+        <input type="email" value={Email} onChange={onEmailHandler}/>
         <label>Password</label>
-        <input type="password" value onChagne />
+        <input type="password" value={Password} onChange={onPasswordHandler} />
 
         <br/>
         <button>
